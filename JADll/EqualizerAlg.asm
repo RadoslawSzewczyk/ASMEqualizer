f11836a47458a20d86fcfe4839687a66564c5ed6
Radek Szewczyk <radekszewczyk@gmail.com>
2025-01-11 23:52:52 +0100

    minor changes

--- JADll/EqualizerAlg.asm
+++ JADll/EqualizerAlg.asm
@@ -1,42 +1,140 @@
-;.586
-;INCLUDE C:\masm32\include\windows.inc
-
-;------------------------------------------------------------------------
-; This assembly procedure processes a buffer of audio samples and applies a gain factor
-; to each sample. The samples are stored as an array of 32-bit integers.
-; Version 0.1
-
-; Input Parameters:
-; RCX - pointer to the input buffer (array of 32-bit integers)
-; RDX - buffer length (number of elements in the array)
-; R8  - gain factor (32-bit integer multiplier)
+.DATA
+M_PI REAL8 3.14159265358979323846
+lowCutoff REAL4 200.0
+highCutoff REAL4 3000.0
+sampleRate REAL4 44100.0
 
 .CODE
+MyProc1 PROC
+    ; Prologue
+    push rbp
+    mov rbp, rsp
+    sub rsp, 512                      ; Reserve more space on the stack
 
-MyProc1 proc                      ; Procedure definition
-    push rbx                      ; Save the current value of rbx to the stack to preserve it
-    xor  rbx, rbx                 ; Clear (set to zero) the rbx register; rbx will serve as our index counter
+    ; Save non-volatile registers
+    push rbx
+    push rsi
+    push rdi
+    push r12
+    push r13
+    push r14
+    push r15
 
-ProcessLoop:
-    cmp  rbx, rdx                 ; Compare index (rbx) with buffer length (rdx)
-    jge  EndLoop                  ; If index >= buffer length, jump to EndLoop and exit
+    ; Load parameters
+    mov r12, rcx                     ; r12 = buffer
+    mov r13, rdx                     ; r13 = length
+    mov r14d, r8d                    ; r14 = lowGain
+    mov r15d, r9d                    ; r15 = midGain
+    mov r10d, r10d                   ; r10 = highGain
 
-    ; Load the current 32-bit sample from the buffer
-    mov eax, [rcx + rbx*4]        ; Load the 32-bit integer sample at position [rcx + rbx*4] into eax
+    ; Constants for filter coefficients
+    vmovss xmm0, DWORD PTR [lowCutoff]    ; xmm0 = lowCutoff
+    vmulss xmm0, xmm0, DWORD PTR [M_PI]  ; xmm0 = 2 * pi * lowCutoff
+    vdivss xmm0, xmm0, DWORD PTR [sampleRate] ; xmm0 = lowAlpha
+    vmovss DWORD PTR [rbp-16], xmm0      ; Store lowAlpha on stack
 
-    ; Apply the gain (multiplication)
-    imul eax, r8d                 ; Multiply the loaded sample (eax) by the gain factor (r8d)
-    
-    ; Store the modified sample back in the buffer
-    mov [rcx + rbx*4], eax        ; Store the result of the multiplication back to the buffer at the same location
+    vmovss xmm1, DWORD PTR [highCutoff]  ; xmm1 = highCutoff
+    vmulss xmm1, xmm1, DWORD PTR [M_PI]  ; xmm1 = 2 * pi * highCutoff
+    vdivss xmm1, xmm1, DWORD PTR [sampleRate] ; xmm1 = highAlpha
+    vmovss DWORD PTR [rbp-20], xmm1      ; Store highAlpha on stack
 
-    ; Increment index for the next iteration
-    inc  rbx                      ; Increment the index counter (rbx)
-    jmp  ProcessLoop              ; Jump back to the start of the loop for the next element
+    ; Initialize filter states
+    xorps xmm2, xmm2                    ; lowYPrev = 0.0
+    xorps xmm3, xmm3                    ; lowXPrev = 0.0
+    xorps xmm4, xmm4                    ; highYPrev = 0.0
+    xorps xmm5, xmm5                    ; highXPrev = 0.0
 
-EndLoop:
-    pop rbx                       ; Restore the original value of rbx from the stack
-    ret                           ; Return from the procedure
-MyProc1 endp                      ; End of procedure definition
+    ; Main loop
+    xor rdi, rdi                       ; rdi = loop counter
+LoopStart:
+    cmp rdi, r13                       ; Compare counter with length
+    jge LoopEnd                        ; Exit if counter >= length
 
-END                               ; Marks the end of the source file
+    ; Load current sample
+    mov eax, DWORD PTR [r12 + rdi*4]   ; eax = buffer[rdi]
+    cvtsi2ss xmm6, eax                 ; xmm6 = (float)buffer[rdi]
+
+    ; Low-pass filter
+    vmovss xmm7, DWORD PTR [rbp-16]    ; xmm7 = lowAlpha
+    vmulss xmm7, xmm7, xmm6            ; xmm7 = lowAlpha * buffer[rdi]
+    vsubss xmm8, xmm2, xmm7            ; xmm8 = (1 - lowAlpha) * lowYPrev
+    vaddss xmm2, xmm7, xmm8            ; lowYPrev = lowAlpha * input + (1 - lowAlpha) * lowYPrev
+    cvtss2si eax, xmm2                 ; Convert lowYPrev to int
+
+    ; Calculate offset for lowPassBuffer
+    lea rbx, [rdi + rdi*2]             ; rbx = rdi * 3
+    shl rbx, 2                         ; rbx = rdi * 12
+
+    ; Store in lowPassBuffer
+    mov DWORD PTR [rsp + rbx], eax     ; Store in lowPassBuffer
+
+    ; High-pass filter
+    vmovss xmm7, DWORD PTR [rbp-20]    ; xmm7 = highAlpha
+    vmulss xmm7, xmm7, xmm6            ; xmm7 = highAlpha * buffer[rdi]
+    vsubss xmm8, xmm4, xmm7            ; xmm8 = highAlpha * (highYPrev + input - highXPrev)
+    vaddss xmm4, xmm7, xmm8            ; highYPrev = highAlpha * input + highAlpha * (highYPrev + input - highXPrev)
+    cvtss2si eax, xmm4                 ; Convert highYPrev to int
+
+    ; Calculate offset for highPassBuffer
+    lea rbx, [rdi + rdi*2]             ; rbx = rdi * 3
+    shl rbx, 2                         ; rbx = rdi * 12
+
+    ; Ensure the offset is within the stack bounds
+    cmp rbx, 508                       ; Compare with stack space (512 - 4)
+    jge LoopEnd                        ; Exit if offset exceeds stack space
+
+    ; Store in highPassBuffer
+    mov DWORD PTR [rsp + rbx + 4], eax ; Store in highPassBuffer
+
+    ; Band-pass filter
+    mov eax, DWORD PTR [r12 + rdi*4]   ; Load original sample
+    sub eax, DWORD PTR [rsp + rbx]     ; Subtract low-pass sample
+    sub eax, DWORD PTR [rsp + rbx + 4] ; Subtract high-pass sample
+
+    ; Store in bandPassBuffer
+    mov DWORD PTR [rsp + rbx + 8], eax ; Store in bandPassBuffer
+
+    ; Apply gains
+    mov eax, DWORD PTR [rsp + rbx]     ; Load low-pass sample
+    imul eax, r14d                     ; Multiply by lowGain
+
+    mov ebx, DWORD PTR [rsp + rbx + 8] ; Load band-pass sample
+    imul ebx, r15d                     ; Multiply by midGain
+    add eax, ebx                       ; Add to low-pass result
+
+    mov ebx, DWORD PTR [rsp + rbx + 4] ; Load high-pass sample
+    imul ebx, r10d                     ; Multiply by highGain
+    add eax, ebx                       ; Add to combined result
+
+    ; Clip to 32-bit integer range
+    cmp eax, 7FFFFFFFh                 ; Compare with INT_MAX
+    jle NoClipHigh                    ; If <= INT_MAX, skip clipping
+    mov eax, 7FFFFFFFh                 ; Set to INT_MAX
+NoClipHigh:
+    cmp eax, 80000000h                 ; Compare with INT_MIN
+    jge NoClipLow                     ; If >= INT_MIN, skip clipping
+    mov eax, 80000000h                 ; Set to INT_MIN
+NoClipLow:
+
+    ; Store result back in buffer
+    mov DWORD PTR [r12 + rdi*4], eax   ; buffer[rdi] = result
+
+    ; Increment loop counter
+    inc rdi
+    jmp LoopStart                     ; Repeat loop
+
+LoopEnd:
+    ; Epilogue
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop rdi
+    pop rsi
+    pop rbx
+    mov rsp, rbp
+    pop rbp
+    ret
+
+MyProc1 ENDP
+END
--- JADll_C/EqualizerAlg.cpp
+++ JADll_C/EqualizerAlg.cpp
@@ -1,8 +1,56 @@
 #include "pch.h"
+#include <cmath>
+#include <vector>
+#include <Windows.h>
 
-extern "C" __declspec(dllexport) void MyProc2(int* buffer, long long length, int gain)
-{
-    for (long long i = 0; i < length; ++i)
-    {
-        buffer[i] *= gain;
+
+constexpr double M_PI{ 3.14159265358979323846 };
+
+// DLL Export Macro
+extern "C" __declspec(dllexport) void MyProc2(int* buffer, long long length, int lowGain, int midGain, int highGain) {
+    // Create buffers for filtered data
+    std::vector<int> lowPassBuffer(length);
+    std::vector<int> bandPassBuffer(length);
+    std::vector<int> highPassBuffer(length);
+
+    // Filter coefficients (example cutoff frequencies)
+    constexpr float lowCutoff{ 200.0f };    // Low-pass cutoff frequency (Hz)
+    constexpr float highCutoff{ 3000.0f };  // High-pass cutoff frequency (Hz)
+    constexpr float sampleRate{ 44100.0f }; // Assuming standard sample rate
+
+    // Simplistic filter coefficients
+    float lowAlpha = 2.0f * M_PI * lowCutoff / sampleRate;
+    float highAlpha = 2.0f * M_PI * highCutoff / sampleRate;
+
+    // Previous samples for low-pass and high-pass filters
+    float lowYPrev = 0.0f;
+    float lowXPrev = 0.0f;
+    float highYPrev = 0.0f;
+    float highXPrev = 0.0f;
+
+    // Process the audio sample by sample
+    for (long long i = 0; i < length; ++i) {
+        // Low-pass filter
+        lowYPrev = (lowAlpha * buffer[i] + (1 - lowAlpha) * lowYPrev);
+        lowPassBuffer[i] = static_cast<int>(lowYPrev);
+
+        // High-pass filter
+        highYPrev = (highAlpha * (highYPrev + buffer[i] - highXPrev));
+        highPassBuffer[i] = static_cast<int>(highYPrev);
+        highXPrev = buffer[i];
+
+        // Band-pass filter (input - low-pass - high-pass)
+        bandPassBuffer[i] = buffer[i] - lowPassBuffer[i] - highPassBuffer[i];
     }
+
+    // Apply gains
+    for (long long i = 0; i < length; ++i) {
+        buffer[i] = (lowPassBuffer[i] * lowGain) +
+            (bandPassBuffer[i] * midGain) +
+            (highPassBuffer[i] * highGain);
+
+        // Clip to ensure the value stays within the 32-bit integer range
+        if (buffer[i] > INT_MAX) buffer[i] = INT_MAX;
+        if (buffer[i] < INT_MIN) buffer[i] = INT_MIN;
+    }
+}
